de37864128d85712ea9369c0ec48ababe11625e8
vkoolko <v.koolko@gmail.com>
2023-11-11 15:21:10 +0300

    универсальный

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 
-
-if __name__ == '__main__':
-    config = "switchport trunk allowed vlan 1,3,10,20,30,100:3434"
+def find_digits(string: str):
+    ''' Поиск целых чисел в строке
+    :param string:  строка в которой надо найти числа
+    :return: список получившихся целых чисел
+    '''
     digits = []
     previous_is_digit = False
-    for s in config:
+    for s in string:
         if s.isdigit():
             if previous_is_digit:
                 index = len(digits) - 1
                 digits[index] = digits[index] + s
             else:
                 digits.append(s)
-
         previous_is_digit = s.isdigit()
+    return [int(n) for n in digits]
 
+
+if __name__ == '__main__':
+    string = "switchport trunk allowed vlan 1,3,10,20,30,100:3434"
+    digits = find_digits(string)
     print(digits)
